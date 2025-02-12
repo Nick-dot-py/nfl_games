9a4212ced9612d087300243433eb122ed58565ac
Nick-dot-py <miller.nick.christa@gmail.com>
2025-02-12 06:45:05 -0600

    Update data_viewer.py

--- data_viewer.py
+++ data_viewer.py
@@ -8,43 +8,58 @@ import pandas as pd
 from tkinter import Tk, ttk, StringVar
 
 
-file_path = r"C:\Users\nick_\OneDrive\Desktop\Python\Projects\Fantasy\Data_Builder\Player_Static.xlsx"
-
-df = pd.read_excel(file_path)
-
 #Create the main window
-root = Tk()
-root.title("Dataframe Viewer")
-
-# Create a StringVar to hold the current cell value
-cell_value = StringVar()
-
-# Create a Treeview widget to display the DataFrame
-tree = ttk.Treeview(root, columns=list(df.columns), show="headings")
-
-# Define Headers
-for col in df.columns:
-    tree.heading(col, text=col)
-
-# Insert data into the Treeview
-for index, row in df.iterrows():
-    tree.insert("", "end", values=list(row))
-
-
-# Function to handle cell selection
-def on_select(event):
-    item = tree.selection()[0]
-    row = tree.item(item)['values']
-    col = tree.identify_column(event.x)[1:] # Get Column Name
-    cell_value.set(row[int(col) - 1]) # Set entry box value
-
+def display_data(WIDTH, HEIGHT, df):
+    new_window = Tk()
+    new_window.title("Dataframe Viewer")
     
-# Bind Events
-tree.bind("<Button-1>", on_select)
+    screen_width = new_window.winfo_screenwidth()
+    screen_height = new_window.winfo_screenheight()
+    WIDTH, HEIGHT = round(screen_width/1.6), round(screen_height/1.6)
+    
+    new_window.geometry(f"{WIDTH}x{HEIGHT}")
+    
+    # Create a StringVar to hold the current cell value
+    cell_value = StringVar()
+    
+    # Create Scrollbar
+    treeScroll_vert = ttk.Scrollbar(new_window)
+    treeScroll_vert.pack(side="right", fill="y")
+    
+    treeScroll_hor = ttk.Scrollbar(new_window, orient='horizontal')
+    treeScroll_hor.pack(side="bottom", fill="x")
+    
+    # Create a Treeview widget to display the DataFrame
+    tree = ttk.Treeview(new_window, columns=list(df.columns), show="headings",
+                        yscrollcommand=treeScroll_vert.set, xscrollcommand=treeScroll_hor.set)
+    
+    # Define Headers
+    for col in df.columns:
+        tree.heading(col, text=col)
+    
+    # Insert data into the Treeview
+    for index, row in df.iterrows():
+        tree.insert("", "end", values=list(row))
+    
+    
+    # Function to handle cell selection
+    def on_select(event):
+        print(tree.selection())
+        item = tree.selection()[0]
+        row = tree.item(item)['values']
+        col = tree.identify_column(event.x)[1:] # Get Column Name
+        cell_value.set(row[int(col) - 1]) # Set entry box value
+    
+        
+    # Bind Events
+    tree.bind("<Button-1>", on_select)
+    tree.selection_set("I001")
+    # Arrange Widgets
+    tree.pack(side="left", fill="both", expand=True)
 
-# Arrange Widgets
-tree.pack(side="left", fill="both", expand=True)
-#entry.pack(side="right", fill="x")
+    treeScroll_vert.config(command=tree.yview)
+    treeScroll_hor.config(command=tree.xview)
+
 
 # Run the application
-root.mainloop()
+#new_window.mainloop()
